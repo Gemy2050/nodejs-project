e838007bd97eb6d1c570a5cbf2c5ae79a9d113a8
Mohamed <gemy46349@gmail.com>
2025-01-28 18:04:41 +0200

    Change Schema

--- models/Article.js
+++ models/Article.js
@@ -1,14 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const articleSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const articleSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: String,
+    numberOfLikes: Number,
   },
-  body: String,
-  numberOfLikes: Number,
-});
+  {
+    toJSON: {
+      transform: (_, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.numberOfLikes;
+        return ret;
+      },
+    },
+  }
+);
 
 const Article = mongoose.model("Article", articleSchema);
 
